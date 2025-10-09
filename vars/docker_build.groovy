e527509ec6c1b2ddc40d2b4bc04f4bec01a0a8df
robinvatshr08 <142587526+robinvatshr08@users.noreply.github.com>
2025-10-09 09:04:14 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(String projectname,String Tag,String dockerhubuser){
- sh "docker build -t ${dockerhubuser}/ ${projectname}:${tag} ." 
+def call(String projectname,String tag,String dockerhubuser){
+ sh "docker build -t ${dockerhubuser}/${projectname}:${tag} ." 
 }
